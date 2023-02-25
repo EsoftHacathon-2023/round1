6161fbdcae77f50f6135446d8c429f6a2cca7c01
Hanifa Rifnaz <hanifarifnazz@gmail.com>
2023-02-25 10:29:56 +0530

    implemented script

--- front-end/script.js
+++ front-end/script.js
@@ -1,4 +1,21 @@
 function btnSubmit(){
-    console.log("123");
+    alert("fidfkdkfskj");
 }
 
+window.onload = () =>
+{
+    (async () => {
+        await fetch("http://127.0.0.1:2001/api/index", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            
+          },
+        }).then(function (response) {
+          if (response.status == 400) {
+            window.location = "./index";
+          }
+          return response.json();
+        });
+      })();
+}
